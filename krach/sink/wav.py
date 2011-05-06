8194b1ab970a077d94f2fdb0ab39a0034f671ebe
Friedrich Weber <fred@reichbier.de>
2011-05-06 18:35:59 +0200

    Better with the wave sink ... we need a threaded model for the ALSA sink.

--- krach/sink/wav.py
+++ krach/sink/wav.py
@@ -3,26 +3,28 @@ import struct
 
 class WaveSink(object):
     def __init__(self, filename, sample_rate=44100, sample_width=2):
+        self.wav = None
         self.filename = filename
         self.sample_rate = sample_rate
         self.sample_width = sample_width
+        self.frame = 0
 
     def sink(self, oscillators):
-        self.wav = wave.open(self.filename, 'w')
-        self.wav.setnchannels(len(oscillators))
-        self.wav.setframerate(self.sample_rate)
-        self.wav.setsampwidth(self.sample_width)
+        if self.wav is None:
+            self.wav = wave.open(self.filename, 'w')
+            self.wav.setnchannels(len(oscillators))
+            self.wav.setframerate(self.sample_rate)
+            self.wav.setsampwidth(self.sample_width)
 
-        frame = 0
-        buf = ''
         sample_width = self.sample_width
         sample_rate = self.sample_rate
         while 1:
-            t = float(frame) / sample_rate
+            t = float(self.frame) / sample_rate
             amplitudes = [osc(t) for osc in oscillators]
             for amp in amplitudes:
                 if amp is None:
                     return # TODO: the right way?
-                self.wav.writeframes(struct.pack('<h', int(amp * (sample_width ** 8) / 2 - 1))) # TODO: sample width
-            frame += 1
+                amp = int(amp * (2 ** (self.sample_width * 8 - 1) - 1))
+                self.wav.writeframes(struct.pack('<h', amp)) # TODO: sample width
+            self.frame += 1
 
--- test.py
+++ test.py
@@ -6,6 +6,6 @@ from krach.controller.sheet import create_sheet, SheetController
 
 sheet = '[c4 e4 g4] [c4 e4 g4] [ab3 c4 eb4] [ab3 c4 eb4]'
 
-sink = AlsaSink(8000)
+sink = WaveSink("yo.wav")
 controller = SheetController(sink, sheet)
 controller.start()
